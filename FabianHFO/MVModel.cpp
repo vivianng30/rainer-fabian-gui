174256f17c65940210b21333facb61b1dd7edefe
Rainer Kuehner <INGBUERO\rku>
2017-01-27 12:16:49 +0000

    5.0.1.5
    git-tfs-id: [http://192.168.178.59:8080/tfs/DefaultCollection]$/FabianHFOProject;C1099

--- FabianHFO/MVModel.cpp
+++ FabianHFO/MVModel.cpp
@@ -107,7 +107,7 @@ CMVModel::CMVModel(void)
 	m_szBuildVersion = _T("9.0.0.0");
 #else
 	m_szVersion = _T("5.0.1");
-	m_szBuildVersion = _T("5.0.1.3");
+	m_szBuildVersion = _T("5.0.1.5");
 #endif
 
 	CTlsRegistry regWorkState(_T("HKCU\\Software\\FabianHFO"),true);
